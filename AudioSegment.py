4f86cd210bd55c1fe52cf5c7f81d7da34a5eacf3
Ars187 <71893695+Ars187@users.noreply.github.com>
2024-03-14 12:37:13 +0530

    update

--- AudioSegment.py
+++ AudioSegment.py
@@ -1,10 +1,11 @@
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 import os
 dir = "D:\\Project\\Dataset\\CwS\\"
 filename = "chunk"
 AudioSegment.ffmpeg = "C:\\Users\\arung\\Downloads\\ffmpeg-2023-10-04-git-9078dc0c52-full_build\\bin\\ffmpeg.exe"
 
-list_files = ["mit035","mit038","mit039","mit049","mit057","mit075","mit088"] #os.listdir("D:\\Project\\Dataset\\CwS\\")
+list_files = os.listdir("D:\\Project\\Dataset\\CwS\\")
 path = "\\audio\\"
 for i in list_files:
     for j in os.listdir(dir + i + path):
@@ -12,9 +13,8 @@ for i in list_files:
         os.makedirs(p)
         audio = AudioSegment.from_mp3(dir + i + path + j)
         audio = audio + 10
-        chunk_length = 8 * 1000 # in milliseconds
-        chunks = [audio[i:i + chunk_length] for i in range(0, len(audio), chunk_length)]
+        chunks = split_on_silence(audio, min_silence_len=2000,  silence_thresh=-40, keep_silence = 500)# Adjust this per requirement
         for k, chunk in enumerate(chunks):
             chunk.export(f"{p}\\{filename}{k}.wav", format="wav")
-
         print(f"Successfully split the audio file into {len(chunks)} chunks.")
+        
